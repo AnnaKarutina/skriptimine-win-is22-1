9386d66b6d7c65a8a24a4694463bd6ce845697fa
Anna Karutina <anna.karutina@voco.ee>
2022-11-07 11:36:53 +0200

    Lokaalse kasutaja loomine etteantud andmetega skriptis

---
+++ lokaalse_kasutaja_loomine.ps1
@@ -0,0 +1,4 @@
+﻿# määrame kasutajale parool, konverteerides turvatud tektiks
+$KasutajaParool = ConvertTo-SecureString 'Parool1!' -AsPlainText -Force
+# loome kasutaja kasutades loodud parooli
+New-LocalUser 'kasutaja1' -Password $KasutajaParool -FullName 'Esmane tavakasutaja' -Description 'Local Account - kasutaja1'
